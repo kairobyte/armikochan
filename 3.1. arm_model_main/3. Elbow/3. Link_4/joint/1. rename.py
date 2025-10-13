bb80b954d2c635ce8c79e5137119625e65eba2b0
Sushant Thakur <mail@sushant.uk>
2025-10-13 09:04:46 +0545

    first commit to new branch after adding files

---
+++ 3.1. arm_model_main/3. Elbow/3. Link_4/joint/1. rename.py
@@ -0,0 +1,22 @@
+import os
+
+header = input("Enter file header: ")
+
+file_name = __file__
+file_name = os.path.basename(file_name)
+
+script_path = os.path.abspath(__file__)
+script_dir = os.path.dirname(script_path)
+os.chdir(script_dir)
+
+files = os.listdir()
+updated_file = []
+
+for file in files:
+    if file == file_name:
+        continue
+    else:
+        os.rename(file, header+file)
+
+files = os.listdir()
+print(files)
